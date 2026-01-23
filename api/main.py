a877fadf51eeef309ac69fd7dc698183217cb8f1
dongwonmoon <ansehddnjs187@gmail.com>
2026-01-24 00:44:16 +0900

    - UTC Aware - 오래된 데이터인 경우 Error raise

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ from contextlib import asynccontextmanager
 import pandas as pd
 import os
 import time
-from datetime import datetime
+from datetime import datetime, timezone
 
 # load_dotenv()
 
@@ -51,34 +51,6 @@ MODELS_DIR = os.path.join(BASE_DIR, "models")
 # 전역 모델 저장소
 loaded_models = {}
 
-# Memo: 모델 로드 코드는 임시로 남겨둠.
-# 1. Worker에서 모델이 로드되는데, Worker이 1분마다 실행됨.
-# 2. 매 실행마다 모델이 로드되는데, 이는 상당히 무거움.
-# 최적화가 필요할 것 같음.
-
-# # 서버 시작 시 모델 미리 로드
-# @app.on_event("startup")
-# async def load_all_models():
-#     print(f"Loading models from {MODELS_DIR}...")
-#     if not os.path.exists(MODELS_DIR):
-#         print("Models directory not found. Skipping model load.")
-#         return
-
-#     for filename in os.listdir(MODELS_DIR):
-#         if filename.endswith(".json") and filename.startswith("model_"):
-#             # 파일명에서 심볼 추출
-#             symbol_part = filename.replace("model_", "").replace(".json", "")
-#             # 파일명에 _가 있으면 /로 복원 (BTC_USDT -> BTC/USDT)
-#             symbol = symbol_part.replace("_", "/")
-
-#             file_path = os.path.join(MODELS_DIR, filename)
-#             try:
-#                 with open(file_path, "r") as fin:
-#                     loaded_models[symbol] = model_from_json(fin.read())
-#                 print(f"Model loaded: {symbol}")
-#             except Exception as e:
-#                 print(f"Failed to load {filename}: {e}")
-
 
 # InfluxDB 쿼리 헬퍼 함수
 def query_influx(symbol: str, measurement: str, days: int = 30):
@@ -108,9 +80,7 @@ def query_influx(symbol: str, measurement: str, days: int = 30):
 
     # InfluxDB 리턴값 정리 ('_time' -> 'timestamp')
     df.rename(columns={"_time": "timestamp"}, inplace=True)
-    df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.tz_localize(
-        None
-    )  # UTC 정보 제거 (JSON 호환)
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
     return df
 
 
@@ -147,14 +117,15 @@ def predict_price(symbol: str):
     # DB에서 예측 결과 조회 (최근 24시간 내 생성된 데이터 중 미래값)
     df = query_influx(symbol, "prediction", days=2)
 
-    if df is None:
+    if df is None or df.empty:
         # DB에 아직 예측값이 없을 경우 (Worker가 안 돌았거나 모델이 없을 때)
-        raise HTTPException(
-            status_code=404, detail="Prediction not ready. Check Pipeline Worker."
-        )
+        raise HTTPException(status_code=404, detail="No prediction data found.")
 
-    now = datetime.utcnow().replace(minute=0, second=0, microsecond=0)
+    now = datetime.now(timezone.utc)
     df = df[df["timestamp"] > now]
+
+    if df is None or df.empty:
+        raise HTTPException(status_code=503, detail="System outdated. Worker is down.")
 
     cols = ["timestamp", "yhat", "yhat_lower", "yhat_upper"]
     available_cols = [c for c in cols if c in df.columns]
